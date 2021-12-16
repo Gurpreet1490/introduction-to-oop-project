2933005e6ee9bdfd60c88073d7ae884bdc9f3a53
Gurpreet1490 <g.preet08092014@gmail.com>
2021-12-16 15:44:45 +0000

    Bird & Main class

--- src/Java/Bird.java
+++ src/Java/Bird.java
@@ -1,11 +1,11 @@
 package Java;
 
-public class Bird {
-
+public class Bird
+{
     protected String name;
     protected String color;
 
-    public Bird(String name, String color){
+    public Bird(String name, String color) {
         this.name = name;
         this.color = color;
     }
@@ -18,10 +18,5 @@ public class Bird {
         System.out.println("I'm " + name + " and I can fly high in the blue sky!");
     }
 
-    public static void main(String[] args) {
-        Bird myBird = new Bird("Tweety", "Yellow");
-        myBird.speak();
-        myBird.fly();
-    }
 }
 
