ae3a42621c1e71a7072373fc37b656f9c3b63c30
Gurpreet1490 <g.preet08092014@gmail.com>
2021-12-16 16:17:32 +0000

    Main class updated

--- src/Java/Main.java
+++ src/Java/Main.java
@@ -2,14 +2,17 @@ package Java;
 
 public class Main {
     public static void main(String[] args) {
-        Bird myBird = new Bird("Tweety", "Yellow");
-        myBird.speak();
-        myBird.fly();
+//        Bird myBird = new Bird("Tweety", "Yellow");
+//        myBird.speak();
+//        myBird.fly();
         Pigeon pippa = new Pigeon("Pippa", "Grey");
         pippa.speak();
         pippa.fly();
         pippa.eatPizza();
         pippa.sleep();
+        Penguin pingu = new Penguin("Pingu", "Black & White");
+        pingu.speak();
+        pingu.fly();
     }
 
 }
